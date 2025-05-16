5c205d42f5bd66f71926608f3b9371eef349438c
mohsen <jason@mohsens-MacBook-Air.local>
2025-05-16 21:34:12 +0330

    feat: implement Queue class with basic operations (enqueue, dequeue, front, isEmpty, printQueue)

--- data-structure/index.js
+++ data-structure/index.js
@@ -57,4 +57,51 @@ class Stack {
 	}
 }
 
-module.exports = { Stack };
+/*
+2 - implement a queue system with the following
+functionalities :
+enqueue(element): Adds an element to the end of the
+queue.
+———
+dequeue(): Removes and returns the front element of
+the queue. If the queue is empty, it should return
+"Underflow".
+———
+front(): Returns the front element of the queue without
+removing it. If the queue is empty, it should return "No
+elements in Queue".
+———
+isEmpty(): Returns true if the queue is empty,
+otherwise returns false.
+———
+printQueue(): Returns a string representation of the
+queue elements, separated by a space.
+*/
+
+class Queue {
+	constructor() {
+		this.items = [];
+	}
+
+	enqueue(element) {
+		this.items.push(element);
+	}
+
+	dequeue() {
+		return this.isEmpty() ? 'Underflow' : this.items.shift();
+	}
+
+	front() {
+		return this.isEmpty() ? 'No elements in Queue' : this.items[0];
+	}
+
+	isEmpty() {
+		return this.items.length === 0;
+	}
+
+	printQueue() {
+		return this.items.join(' ');
+	}
+}
+
+module.exports = { Stack, Queue };
