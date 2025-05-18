06310f6ba4c156acd48a00e0445e009237e9f53c
mohsen <mirmohsenmirazimitarki78@gmail.com>
2025-05-18 19:29:58 +0330

    implement controller signin

--- task1/controller/auth.js
+++ task1/controller/auth.js
@@ -1,15 +1,17 @@
-import { createUser, existUser } from '../model/user.js';
+import { createUser, existUser, getUserByEmail } from '../model/user.js';
+import { comparePassword } from '../utils/encryption.js';
+import { userSignupSchema, userSigninSchema } from '../utils/validation.js';
 
 export const signup = async (req, res) => {
 	const { email, username, password } = req.body;
 
+	const { error } = userSignupSchema.validate({ email, username, password });
+	if (error) {
+		return res.status(400).json({ message: error.details[0].message });
+	}
+
 	try {
-		if (!isValidEmail(email)) {
-			return res.status(400).json({ message: 'Invalid email format' });
-		}
-
 		const userExists = await existUser(email);
-
 		if (userExists) {
 			return res
 				.status(400)
@@ -22,6 +24,34 @@ export const signup = async (req, res) => {
 			.json({ message: 'User created successfully', userId: user._id });
 	} catch (err) {
 		console.error('Signup error:', err);
+		return res.status(500).json({ message: 'Internal server error' });
+	}
+};
+
+export const signin = async (req, res) => {
+	const { email, password } = req.body;
+
+	const { error } = userSigninSchema.validate({ email, password });
+	if (error) {
+		return res.status(400).json({ message: error.details[0].message });
+	}
+
+	try {
+		let userExist = await getUserByEmail(email);
+		if (!userExist) {
+			return res.status(401).json({ message: 'Invalid email or password' });
+		}
+
+		const isMatch = await comparePassword(password, userExist.password);
+		if (!isMatch) {
+			return res.status(401).json({ message: 'Invalid email or password' });
+		}
+
+		return res
+			.status(200)
+			.json({ message: 'Signin successful', userId: userExist._id });
+	} catch (err) {
+		console.error('Signin error:', err);
 		return res.status(500).json({ message: 'Internal server error' });
 	}
 };
