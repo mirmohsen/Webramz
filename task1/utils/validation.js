038487047d079d2c226f27b948956f4303aa5431
mohsen <mirmohsenmirazimitarki78@gmail.com>
2025-05-18 19:27:51 +0330

    create userSignupSchema and userSigninSchema for check validation inputs

--- task1/utils/validation.js
+++ task1/utils/validation.js
@@ -1,4 +1,17 @@
+import Joi from 'joi';
+
 export const isValidEmail = (email) => {
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	return emailRegex.test(email);
 };
+
+export const userSignupSchema = Joi.object({
+	email: Joi.string().email().lowercase().trim().required(),
+	username: Joi.string().min(3).max(30).trim().required(),
+	password: Joi.string().min(6).max(128).required(),
+});
+
+export const userSigninSchema = Joi.object({
+	email: Joi.string().email().lowercase().trim().required(),
+	password: Joi.string().min(6).max(128).required(),
+});
