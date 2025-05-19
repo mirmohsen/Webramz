6defa320caf270637d80d6537d56c7e34ed813ad
mohsen <jason@mohsens-MacBook-Air.local>
2025-05-19 13:14:28 +0330

    implement authenticate method for check authoraization

--- task1/utils/jwt.js
+++ task1/utils/jwt.js
@@ -12,3 +12,18 @@ export const generateToken = (payload) => {
 export const verifyToken = (token) => {
 	return jwt.verify(token, JWT_SECRET);
 };
+
+export const authenticate = (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader?.startsWith('Bearer '))
+		return res.status(401).json({ message: 'Unauthorized: No token provided' });
+
+	const token = authHeader.split(' ')[1];
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
+	} catch (err) {
+		return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+	}
+};
